312e8a7f3389bbfc48566e3548cc31b5de561cfb
SamuelMoraisdRS <samuelmorais552@gmail.com>
2024-04-02 13:23:09 -0300

    Retorno do resultado em string

--- lib/first_project.ex
+++ lib/first_project.ex
@@ -14,61 +14,44 @@ defmodule FirstProject do
   """
 
 
-  # Power function using Decimal library's numbers
+  # Função exponenciação
   defp power(0,_) do
     0
   end
-
   defp power(_,0) do
     1
   end
-
   defp power(x, e) do
     Decimal.mult(x, power(x,e-1))
   end
 
-  defp calculateTerm(x,0) do
+  #Calcula cada termo do somatório
+  defp calculateTerm(x,0) do # Caso base
     x
   end
-
   defp calculateTerm(x, idx) do
     exp = (idx*2) + 1
     listEven = 2..(exp - 1)//2
     listOdds = 1..(exp-1)//2
     numerator = Enum.reduce(listOdds, 1, fn (x,y) -> x * y end)
-    IO.puts("numerator:")
-    IO.puts(numerator)
     denominator = Enum.reduce(listEven, 1, fn (x,y) -> x * y end)
-    IO.puts("denominator")
-    IO.puts(denominator)
     a = Decimal.div(numerator, denominator)
-    IO.puts(Decimal.mult(a, Decimal.div(Decimal.mult(x,exp),exp)))
     Decimal.mult(a, Decimal.div(power(x,exp),exp))
   end
 
-  # This version cannot be called by the client
+  # Função correspondente ao somatório
   defp calculateArcSin(x,epsilon, idx, sum) do
     a = calculateTerm(x, idx)
-    # IO.puts("Termo calculado")
-    # IO.puts(a)
     s = Decimal.add(a, sum)
-    # IO.puts("epsilon")
-    # IO.puts(epsilon)
-    # IO.puts("diff")
-    # IO.puts(Decimal.sub(s,sum))
-    # IO.puts(Result)
-    # IO.puts(s)
     if Decimal.lt?((Decimal.sub(s,sum)),epsilon) do
-      s
+      Decimal.to_string(s)
     else
     calculateArcSin(x, epsilon, idx + 1, s)
   end
   end
+
   def calculateArcSin(x, epsilon) do
-    Decimal.Context.set(%Decimal.Context{Decimal.Context.get() | precision: 500})
-    # curr_term = calculateTerm(x,0)
-    # # Pode resultar em 2x
-    # s = Decimal.add(x, curr_term)
+    Decimal.Context.set(%Decimal.Context{Decimal.Context.get() | precision: 600})
     if Decimal.lt?((Decimal.sub(x,x)),epsilon) do
       x
     end
