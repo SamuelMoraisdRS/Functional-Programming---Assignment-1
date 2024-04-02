4dddb0e7b93b44785b371a7d42d4993e74255972
SamuelMoraisdRS <samuelmorais552@gmail.com>
2024-04-02 14:31:43 -0300

    Otimização na função de chamada inicial

--- lib/first_project.ex
+++ lib/first_project.ex
@@ -26,15 +26,17 @@ defmodule FirstProject do
   end
 
   #Calcula cada termo do somatório
-  defp calculateTerm(x,0) do # Caso base
+  defp calculateTerm(x,0) do
     x
   end
   defp calculateTerm(x, idx) do
     exp = (idx*2) + 1
+    # Pode ser otimizado {
     listEven = 2..(exp - 1)//2
     listOdds = 1..(exp-1)//2
     numerator = Enum.reduce(listOdds, 1, fn (x,y) -> x * y end)
     denominator = Enum.reduce(listEven, 1, fn (x,y) -> x * y end)
+    # }
     a = Decimal.div(numerator, denominator)
     Decimal.mult(a, Decimal.div(power(x,exp),exp))
   end
@@ -44,14 +46,15 @@ defmodule FirstProject do
     a = calculateTerm(x, idx)
     s = Decimal.add(a, sum)
     if Decimal.lt?((Decimal.sub(s,sum)),epsilon) do
-      Decimal.to_string(s)
+      # Decimal.to_string(s)
+      s
     else
     calculateArcSin(x, epsilon, idx + 1, s)
   end
   end
 
-  def calculateArcSin(x, epsilon) do
-    Decimal.Context.set(%Decimal.Context{Decimal.Context.get() | precision: 600})
+  def calculate(x, epsilon) do
+    Decimal.Context.set(%Decimal.Context{Decimal.Context.get() | precision: 6000})
     if Decimal.lt?((Decimal.sub(x,x)),epsilon) do
       x
     end
